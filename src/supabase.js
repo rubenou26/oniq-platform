6792d2baba816c4e76c57a00711120b1a9753507
rubenou26 <129214507+rubenou26@users.noreply.github.com>
2026-03-09 22:38:05 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://kgkohfhcqjueufgoqaze.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtna29oZmhjcWp1ZXVmZ29xYXplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0NzA3ODcsImV4cCI6MjA1NzA0Njc4N30.HnFLeq0VcDkOdFxMEQi9DPiFxfQqmGpHQoY2Dmt4fHU'
+const SUPABASE_KEY = 'sb_publishable_fpHIFud2upSH2DZS0OWM5g_wuG9erqz'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true
+  }
+})
