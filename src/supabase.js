cfefdd0b3b16515af3bda63fea99f36ef489e914
rubenou26 <129214507+rubenou26@users.noreply.github.com>
2026-03-09 20:10:04 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://kgkohfhcqjueufgoqaze.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_fpHIFud2upSH2DZS0OWM5g_wuG9erqz'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtna29oZmhjcWp1ZXVmZ29xYXplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMTQzOTAsImV4cCI6MjA4ODU5MDM5MH0.DULKNkfOZ3cqudqvXhJFKlI0KErPuPbmTFv75yObQTs'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
